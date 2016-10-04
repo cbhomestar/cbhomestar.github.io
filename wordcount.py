dd0becd110b96bffcf6b2c6db35cd91dce930d47
cbhomestar <cody.burt.21@gmail.com>
2016-10-03 23:30:07 -0600

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -19,32 +19,40 @@ from __future__ import print_function
 
 import sys
 from operator import add
+from collections import defaultdict
 
 from pyspark.sql import SparkSession
 
-def removePunc(word):
-  word = word.replace(".", "")
-  word = word.replace(",", "")
-  word = word.replace(":", "")
-  word = word.replace(";", "")
-  word = word.replace("!", "")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: wordcount <file>", file=sys.stderr)
+    if len(sys.argv) != 4:
+        print("Usage: netflix <file> <userID> <output>", file=sys.stderr)
         exit(-1)
 
     spark = SparkSession\
         .builder\
-        .appName("PythonWordCount")\
+        .appName("NetflixRecommendation")\
         .getOrCreate()
 
     lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
-    counts = lines.flatMap(lambda x: x.split(' ')) \
-                  .map(lambda x: (removePunc(x), 1)) \
-                  .reduceByKey(add)
-    output = counts.collect()
-    for (word, count) in output:
-        print("%s: %i" % (word, count))
+    movieRatingsToUsers = lines.map(lambda x: (x[1]+"-"+x[2], x[0])) \
+                  .groupByKey().mapValues(list)
+
+    usersToRatings = lines.map(lambda x: (x[0], x[1]+"-"+x[2])) \
+                  .groupByKey().mapValues(list)
+    userMovies = usersToRatings.lookup(sys.argv[2])
+
+    similar = defaultdict(int)
+    for movie in userMovies:
+      usersThatRatedSame = movieRatingsToUsers.lookup(movie)
+      for user in usersThatRatedSame:
+        similar[user] += 1
+
+    similar = similar.items()
+    similar.sort(reverse=True)
+
+    target = open(sys.argv[3], 'w')
+    for similarUser in similar:
+      target.write(similarUser[0])
+      target.write("\n")
 
     spark.stop()
